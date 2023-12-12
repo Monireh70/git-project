2067cfd8c0edc1ec7dbdd2cf68f570a0dca571ac
Monir <monireh.taghilo@gmail.com>
2023-12-12 12:59:37 +0330

    initializeation

---
+++ src/pages/Profile.jsx
@@ -0,0 +1,12 @@
+function Profile() {
+  return (
+    <div className="profile">
+      <header class="header">header</header>
+      <div className="main">
+        <div className="sidebar">this is sidebar</div>
+        <div className="activity">this is activity</div>
+      </div>
+    </div>
+  );
+}
+export default Profile;
